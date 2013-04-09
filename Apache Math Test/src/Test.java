7c7bb9516d9858812cfa030d34d9c913b96b1278
Manjusri Ishwara <manju@ishwara.sl>
2013-04-09 21:44:16 +0800

    check

--- Apache Math Test/src/Test.java
+++ Apache Math Test/src/Test.java
@@ -1,4 +1,5 @@
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 
@@ -19,7 +20,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		// Create a real matrix with two rows and three columns
-		double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
+/*		double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
 		RealMatrix m = new Array2DRowRealMatrix(matrixData);
 
 		// One more with three rows, two columns
@@ -32,11 +33,21 @@ public class Test {
 		RealMatrix p = m.multiply(n);
 		System.out.println(p.toString());    // 2
 		System.out.println(p.getColumnDimension()); // 2
-
+*/
 		// Invert p, using LU decomposition
-		RealMatrix pInverse = new LUDecomposition(p).getSolver().getInverse();
-		System.out.println(pInverse.toString());
-		System.out.println("D8D");
+	//	RealMatrix pInverse = new LUDecomposition(p).getSolver().getInverse();
+/*		System.out.println(pInverse.toString());
+		System.out.println("D8D");*/
+		
+		double temp[] = {5.0,6.0,2.0};
+		
+		ArrayRealVector arv = new ArrayRealVector(temp);
+		
+		System.out.print(arv.toString());
+		
+		ArrayRealVector lol = (ArrayRealVector) arv.mapMultiplyToSelf(5);
+		
+		System.out.print(lol.toString());
 	}
 
 }
