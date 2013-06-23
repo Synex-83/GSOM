31c4bf1d73a071b2a21a968117d701ba8be29edb
Manjusri Ishwara <manju@ishwara.sl>
2013-06-23 22:15:28 +0800

    basic fixes to the UI bug

--- Organizing Maps/src/maps/Structures/DisplayLattice.java
+++ Organizing Maps/src/maps/Structures/DisplayLattice.java
@@ -3,71 +3,117 @@
  */
 package maps.Structures;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-import javax.swing.JPanel;
-
-/**
- * @author User
- *
- */
-public class DisplayLattice extends JPanel {
-
-	private BufferedImage IMAGE = IMAGE = new BufferedImage(400, 400, 1);;
+@SuppressWarnings("serial")
+public class DisplayLattice extends JPanel  {
 	
-	public DisplayLattice()
-	{
+	private BufferedImage img = new BufferedImage(500, 500, 1);	
 		
-		//repaint();
-	}
-	
-
-	public void paintComponent(Graphics g) {	
-		if (IMAGE == null)
+	public void paintComponent(Graphics g) {
+		if (img == null)
 			super.paintComponents(g);
 		else
-			g.drawImage(IMAGE, 0, 0, this);
+			g.drawImage(img, 0, 0, this);
 	}
 	
-	public BufferedImage getImage()
-	{
-		if(IMAGE == null)
-				IMAGE = (BufferedImage)createImage(getWidth(), getHeight());
-		
-		return IMAGE;
-	}
-	
+
 	public void render() {
 
-	int cellWidth = 50;
-	int cellHeight = 50;
+	float cellWidth = 100;
+	float cellHeight = 100;
 		
-		int imgW = IMAGE.getWidth();
-		int imgH = IMAGE.getHeight();
-		
-		Graphics2D g2 = IMAGE.createGraphics();
+		int imgW = img.getWidth();
+		int imgH = img.getHeight();
+		float r, g, b;
+		Graphics2D g2 = img.createGraphics();
 		g2.setBackground(Color.black);
-		g2.clearRect(0,0,IMAGE.getWidth(),IMAGE.getHeight());
-		
-		for (int x=0; x<imgW; x++) {
-			for (int y=0; y<imgH; y++) {
-				//g2.setColor(new Color(IMAGE.getRGB(x, y)));
-				g2.setColor(new Color((float)Math.random(),(float)Math.random(),(float)Math.random()));
+		g2.clearRect(0,0,imgW,imgH);
+		for (int x=0; x<100; x++) {
+			for (int y=0; y<100; y++) {
+				r = (float)Math.random();
+				g = (float)Math.random();
+				b = (float)Math.random();
+				g2.setColor(new Color(r,g,b));
 				g2.fillRect((int)(x*cellWidth), (int)(y*cellHeight),
-							(int)cellWidth, (int)cellHeight);
+							(int)cellWidth+1, (int)cellHeight+1);
 			}
 		}
 		g2.setColor(Color.black);
 		g2.dispose();
 		repaint();
-	}	
+	}
 	
-	public void setImage(BufferedImage image)
+	public void render2(BufferedImage buff)
 	{
-		IMAGE = image;
+		setImage(buff);
 		repaint();
+	}
+		
+	public void renderb() {
+
+	float cellWidth = 10;
+	float cellHeight = 10;
+		
+		int imgW = img.getWidth();
+		int imgH = img.getHeight();
+		float r, g, b;
+		Graphics2D g2 = img.createGraphics();
+		g2.setBackground(Color.black);
+		g2.clearRect(0,0,imgW,imgH);
+		for (int x=0; x<100; x++) {
+			for (int y=0; y<100; y++) {
+				r = (float)Math.random();
+				g = (float)Math.random();
+				b = (float)Math.random();
+				g2.setColor(new Color(0,0,b));
+				g2.fillRect((int)(x*cellWidth), (int)(y*cellHeight),
+							(int)cellWidth+1, (int)cellHeight+1);
+			}
+		}
+		g2.setColor(Color.black);
+		g2.dispose();
+		repaint();
+	}
+	
+	public void renderg() {
+
+	float cellWidth = 10;
+	float cellHeight = 10;
+		
+		int imgW = img.getWidth();
+		int imgH = img.getHeight();
+		float r, g, b;
+		Graphics2D g2 = img.createGraphics();
+		g2.setBackground(Color.black);
+		g2.clearRect(0,0,imgW,imgH);
+		for (int x=0; x<100; x++) {
+			for (int y=0; y<100; y++) {
+				r = (float)Math.random();
+				g = (float)Math.random();
+				b = (float)Math.random();
+				g2.setColor(new Color(0,g,0));
+				g2.fillRect((int)(x*cellWidth), (int)(y*cellHeight),
+							(int)cellWidth+1, (int)cellHeight+1);
+			}
+		}
+		g2.setColor(Color.black);
+		g2.dispose();
+		repaint();
+	}
+	
+	public BufferedImage getImage() {
+		if (img == null)
+			img = (BufferedImage)createImage(500, 500);
+		
+		return img;
+	}
+	
+	public void setImage(BufferedImage bimg) {
+		img = bimg;
 	}
 }
