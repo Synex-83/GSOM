2472b986a5307c0d5179f88b0e2cbba767eca120
Manjusri Ishwara <manju@ishwara.sl>
2013-06-24 21:39:15 +0800

    Gsom basic template

--- Organizing Maps/src/maps/Structures/GSOMNode.java
+++ Organizing Maps/src/maps/Structures/GSOMNode.java
@@ -13,11 +13,11 @@ public class GSOMNode extends Node {
 
 	private double TOTAL_ERROR = 0.0;
 	private boolean IS_BOUNDRY = false;
+	private GSOMNode LEFT,RIGHT,UP,DOWN;
 	
 
 	public GSOMNode(int Dimensions, int x, int y) {
 		super(Dimensions, x, y);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public double getTOTAL_ERROR() {
@@ -35,4 +35,61 @@ public class GSOMNode extends Node {
 	public void setBoundry(boolean isBoundry) {
 		IS_BOUNDRY = isBoundry;
 	}
+
+	/**
+	 * @return the lEFT
+	 */
+	public GSOMNode getLEFT() {
+		return LEFT;
+	}
+
+	/**
+	 * @param lEFT the lEFT to set
+	 */
+	public void setLEFT(GSOMNode left) {
+		LEFT = left;
+	}
+
+	/**
+	 * @return the rIGHT
+	 */
+	public GSOMNode getRIGHT() {
+		return RIGHT;
+	}
+
+	/**
+	 * @param rIGHT the rIGHT to set
+	 */
+	public void setRIGHT(GSOMNode right) {
+		RIGHT = right;
+	}
+
+	/**
+	 * @return the uP
+	 */
+	public GSOMNode getUP() {
+		return UP;
+	}
+
+	/**
+	 * @param uP the uP to set
+	 */
+	public void setUP(GSOMNode up) {
+		UP = up;
+	}
+
+	/**
+	 * @return the dOWN
+	 */
+	public GSOMNode getDOWN() {
+		return DOWN;
+	}
+
+	/**
+	 * @param dOWN the dOWN to set
+	 */
+	public void setDOWN(GSOMNode down) {
+		DOWN = down;
+	}
+	
 }
--- Organizing Maps/src/maps/Structures/GrowingSelfOrganizingMap.java
+++ Organizing Maps/src/maps/Structures/GrowingSelfOrganizingMap.java
@@ -4,7 +4,7 @@
 package maps.Structures;
 
 /**
- * @author 		User
+ * @author 		Manjusri Ishwara
  * @date		Apr 29, 2013 - 4:35:45 PM
  * @type        GrowingSelfOrganizingMap
  *
