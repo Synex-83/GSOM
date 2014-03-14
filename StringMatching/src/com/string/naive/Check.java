65b5ec787373f7877f1be7c19fe97c8bef7b3881
Manjusri Ishwara <manju@ishwara.sl>
2014-03-15 03:14:06 +0800

    time update

--- StringMatching/src/com/string/naive/Check.java
+++ StringMatching/src/com/string/naive/Check.java
@@ -17,24 +17,30 @@ public class Check {
 		// TODO Auto-generated method stub
 		FileProcessing fp = null;
 		
-		fp = new FileProcessing("E:\\workspace\\GSOM\\Sequence Modeling\\data\\mtDNA\\human - D38112.txt", 1); 
-		String s1 = fp.readFileLine(); 
+		//fp = new FileProcessing("E:\\workspace\\GSOM\\Sequence Modeling\\data\\mtDNA\\human - D38112.txt", 1); 
+		//String s1 = fp.readFileLine(); 
 				
 	
-		fp = new FileProcessing("E:\\workspace\\GSOM\\Sequence Modeling\\data\\mtDNA\\chimpanzee - D38113.txt", 1); 
+		fp = new FileProcessing("C:\\Users\\Manjusri\\Desktop\\2.txt", 1); 
 		String s2 = fp.readFileLine(); 
-		
+		System.out.println("HERE");
 		String p = "ACCTATC";
 		
-		NaiveStringMatching nv = null; 
+	//	NaiveStringMatching nv = null; 
 		
-		System.out.println("========= D38112 ==========");
+/*		System.out.println("========= D38112 ==========");
 		nv = new NaiveStringMatching(s1, p);
 		nv.count();
 		
 		System.out.println("========= D38113 ==========");
 		nv = new NaiveStringMatching(s2, p);
-		nv.count();
+		nv.count();*/
+		
+		String g[] = {"CT","TG","GA","TC","AG","GC","CC","GT","GG","TT","CG"};
+		
+		FilterSolid ft = new FilterSolid(g, s2);
+		
+		ft.process();
 	}
 
 }
