3efce1b2a009228929fb18e162df006f0c06dad9
Manjusri Ishwara <manju@ishwara.sl>
2014-04-24 21:35:10 +0800

    time update and data update

--- Sequence Modeling/src/maps/TestClasses/TestSOM.java
+++ Sequence Modeling/src/maps/TestClasses/TestSOM.java
@@ -34,11 +34,11 @@ public class TestSOM {
 		
 		//E:\\workspace\\GSOM\\Sequence Modeling\\data\\mtDNA\\Sets\\X83427.txt
 		//
-		fp = new FileProcessing("E:\\workspace\\GSOM\\Sequence Modeling\\data\\Pacman\\W-File\\15w.txt", 1); 
-		length = 1252;
-		threshold = (int)Math.round((length/159.40)*(100)*(0.5));
+		fp = new FileProcessing("E:\\workspace\\GSOM\\Sequence Modeling\\data\\Pacman\\G-File\\2g.txt", 1); 
+		length = 855;
+		threshold = (int)Math.round((length/162.10)*(100)*(1.0));
 		
-		System.out.println("*******************            15w            ****************************");
+		System.out.println("*******************            2g            ****************************");
 
 		SelfOrganizingMap SOM = new SelfOrganizingMap(10000,fp.getDataDimension(),true,2,1000,0,4); //null; //
 		
